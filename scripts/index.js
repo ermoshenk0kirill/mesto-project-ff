d5de0c83c80e9d35297cc3d2e34fe27961ba36fe
Kirill Ermoshenko <ermoshenkokirill@yandex.ru>
2024-09-25 23:33:51 +0300

    Версия сайта №1

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,31 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const cardTemplate = document.querySelector("#card-template").content;
+const cardList = document.querySelector(".places__list");
+
+function createCard(linkValue, titleValue) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__image").src = linkValue;
+  cardElement.querySelector(".card__image").alt = titleValue;
+  cardElement.querySelector(".card__title").textContent = titleValue;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  deleteButton.addEventListener("click", function () {
+    const cardItem = deleteButton.closest(".card");
+    cardItem.remove();
+  });
+
+  return cardElement;
+}
+
+function addCard() {
+  initialCards.forEach(function (cardValue) {
+    const newCard = createCard(cardValue.link, cardValue.name);
+    cardList.append(newCard);
+  });
+}
+
+addCard();
